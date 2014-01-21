fc29da146c18c176714d8e6fa0144b961c3f2edc
Ravishekhar Banger <ravibanger@gmail.com>
2014-01-21 15:57:21 +0530

    reverting the code

--- test/cl/ConstantIteratorTest/ConstantIteratorTest.cpp
+++ test/cl/ConstantIteratorTest/ConstantIteratorTest.cpp
@@ -21,7 +21,6 @@
 
 #include "bolt/cl/iterator/constant_iterator.h"
 #include "bolt/cl/iterator/counting_iterator.h"
-#include "bolt/cl/iterator/transform_iterator.h"
 #include "bolt/cl/transform.h"
 #include "bolt/cl/reduce.h"
 #include "bolt/cl/functional.h"
